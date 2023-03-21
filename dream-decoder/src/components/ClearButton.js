cf50ff456e92cad8d57d75786dd2dd49be5aa8b1
Joseph Douglas <joecdouglas2@gmail.com>
2023-03-21 20:08:32 +0000

    Changes to be committed: 	modified:   dream-decoder/src/components/ClearButton.js

--- dream-decoder/src/components/ClearButton.js
+++ dream-decoder/src/components/ClearButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /**
- * A component that renders a button to clear the dream input and output fields.
+ * A component that renders a button to clear the dream input and output fields. 
  * @param {function} onClick - A callback function to be called when the user clicks the clear button.
  */
 function ClearButton({ onClick }) {
